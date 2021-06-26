fc1c9ba4f0196eb96c175aa741727002eda9757a
e2195479 <86504934+e2195479@users.noreply.github.com>
2021-06-26 15:42:34 -0400

    Add files via upload

---
+++ index_Loundja.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html >
+< html  lang =" fr " >
+  < tête >
+    < meta  charset =" UTF-8 " />
+    < meta  name =" viewport " content =" width=device-width, initial-scale=1.0 " />
+    < meta  name =" description " content =" Page de test pour Git " />
+    < meta  name =" author " content =" Collectif de 4 étudiants " />
+
+    < title > Test pour Git </ title >
+  </ tête >
+  < corps >
+    < h3 > 420-11E-MA - Environnement de développement Web 1 </ h3 >
+    < div > Loundja Hamis </ div >
+  </ corps >
+</ html >
---
+++ index_Loundja.php
@@ -0,0 +1,4 @@
+
+   420-11E-MA - Environnement de développement Web 1 
+   Loundja Hamis 
+  
